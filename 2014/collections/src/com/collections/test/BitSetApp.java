2a62cdf784bef3e9ff94223afa650a9705c0b7b8
Srinivas <mrsrinivas@live.com>
2014-10-16 23:24:25 +0530

    Collections added
    Linkedlist, BitSet and TreeSet has been added

---
+++ 2014/collections/src/com/collections/test/BitSetApp.java
@@ -0,0 +1,32 @@
+package com.collections.test;
+
+/**
+ * @author	Srinivas Reddy
+ * @email	srinivas96alluri@gmail.com
+ */
+
+import java.util.Random;
+
+import com.collections.BitSet;
+
+public class BitSetApp {
+	public static void main(String[] args) {
+
+		BitSet bitSet = new BitSet(256);
+
+		Random rand = new Random();
+
+		for (int index = 1; index <= 20; ++index) {
+			int random = rand.nextInt(20);
+
+			System.out.println("Ramdom numnber is = " + random);
+
+			if (bitSet.get(random)) {
+				System.out.println("\nDuplicate found = " + random);
+				break;
+			} else {
+				bitSet.set(random);
+			}
+		}
+	}
+}
