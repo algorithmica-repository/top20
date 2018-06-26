08f4b9c55da57aad67a10f11cb503af15026ed03
Algorithmica <info@algorithmica.co.in>
2018-06-26 16:38:06 +0530

    class code

---
+++ 2018-may/13.complete search/src/AllSequences.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+
+public class AllSequences {
+
+	public static void allSeq(char[] in, int n){
+		char[] out = new char[n];
+		auxSeq(0, n, in, out);		
+	}
+	private static void auxSeq(int d, int n, char[] in, char[] out) {
+		if(d == n) {
+			System.out.println(Arrays.toString(out));
+			return;
+		}
+		for(int i = 0; i < in.length; ++i) {
+			out[d] = in[i];
+			auxSeq(d+1, n, in, out);
+		}
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		char[] in = args[1].toCharArray();
+		allSeq(in, n);
+	}
+
+
+}
---
+++ 2018-may/13.complete search/src/TernarySeq.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+
+public class TernarySeq {
+
+	public static void ternary1(int n){
+		for(int i = 0; i <= 2; ++i) {
+			for(int j = 0; j <= 2; ++j) {
+				for(int k = 0; k <= 2; ++k) {
+					System.out.println(i+" "+j+" " +k);
+				}
+			}
+		}
+	}
+	
+	public static void ternary2(int n){
+		int[] out = new int[n];
+		auxTernary(0, n, out);		
+	}
+	private static void auxTernary(int d, int n, int[] out) {
+		if(d == n) {
+			System.out.println(Arrays.toString(out));
+			return;
+		}
+		for(int i = 0; i <= 2; ++i) {
+			out[d] = i;
+			auxTernary(d+1, n, out);
+		}
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		ternary2(n);
+	}
+
+}
