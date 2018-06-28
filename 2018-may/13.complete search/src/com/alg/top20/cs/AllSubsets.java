d5cd26f1072806e48cf281aca803945918c718a8
Algorithmica <info@algorithmica.co.in>
2018-06-28 21:41:23 +0530

    class code

--- 2018-may/13.complete search/src/com/alg/top20/cs/AllSubsets.java
+++ 2018-may/13.complete search/src/com/alg/top20/cs/AllSubsets.java
@@ -15,18 +15,16 @@ public class AllSubsets {
 	}
 	
 	public static void allSubsets2(String s) {
-		auxSubsets1(0, s, "");
+		auxSubsets2(s, "");
 	}
-	private static void auxSubsets2(int d, String s, String path) {
-		if(d == s.length()) {
-			System.out.println(path);
-			return;
-		}
-		auxSubsets1(d+1, s, path);
-		auxSubsets1(d+1, s, path+s.charAt(d));
+	private static void auxSubsets2(String s, String path) {
+		System.out.println(path);
+		if(s.length() == 0) return;
+		for(int i = 0; i < s.length(); ++i) 
+			auxSubsets2(s.substring(i+1), path+s.charAt(i));
 	}
 	public static void main(String[] args) {
-		allSubsets1(args[0]);
+		allSubsets2(args[0]);
 	} 
 
 }
---
+++ 2018-may/14.backtracking/src/Permutations.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+
+public class Permutations {
+
+	private static boolean isValid1(String path) {
+		for(int i = 0; i < path.length(); ++i) {
+			for(int j = i+1; j < path.length(); ++j) {
+				if(path.charAt(i) == path.charAt(j))
+					return false;
+			}
+		}
+		return true;
+	}
+	public static void permutation1(String in){
+		auxPermutation1(0, in, "");		
+	}
+	private static void auxPermutation1(int d, String in, String path) {
+		if(d == in.length()) {
+			if(isValid1(path)) 
+				System.out.println(path);
+			return;
+		}
+		for(int i = 0; i < in.length(); ++i) 
+			auxPermutation1(d+1, in, path + in.charAt(i));
+	}
+	
+	private static boolean isValid2(String path, char c) {
+		if(path.contains(c+"")) return false;
+		return true;
+	}
+	public static void permutation2(String in){
+		auxPermutation2(0, in, "");		
+	}
+	private static void auxPermutation2(int d, String in, String path) {
+		if(d == in.length()) {
+			System.out.println(path);
+			return;
+		}
+		for(int i = 0; i < in.length(); ++i) {
+			if(isValid2(path, in.charAt(i))) 
+				auxPermutation2(d+1, in, path + in.charAt(i));
+		}
+	}
+	public static void main(String[] args) {
+		permutation1(args[0]);
+	}
+
+
+}
