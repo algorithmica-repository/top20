a7e1933055e1b4369e4c00c806321e085ea5bad5
Algorithmica <info@algorithmica.co.in>
2019-05-30 10:04:01 +0530

    Add files via upload

--- 2019-may/3.linked list problems/linked list problems/src/com/alg/top20/ll/CycleDetection.java
+++ 2019-may/3.linked list problems/linked list problems/src/com/alg/top20/ll/CycleDetection.java
@@ -3,8 +3,23 @@ package com.alg.top20.ll;
 import java.util.HashSet;
 
 public class CycleDetection {
-
+	
+	//TC:O(n^2)  SC:O(1)
 	public static boolean hasCycle1(ListNode head) {
+		int l = 1;
+		for(ListNode current = head.next; current != null; current = current.next) {
+			ListNode tmp = head;
+			for(int i = 1; i <= l; ++i)  {
+				if(tmp == current) return true;
+				tmp = tmp.next;
+			}
+			l = l + 1;
+		}
+		return false;
+	}
+	
+	//TC:O(n)  SC:O(n)
+	public static boolean hasCycle2(ListNode head) {
 		HashSet<ListNode> hset = new HashSet<ListNode>();
 		ListNode current = head.next;
 		while (current != null) {
@@ -15,15 +30,16 @@ public class CycleDetection {
 		}
 		return false;
 	}
-	
-	public static boolean hasCycle2(ListNode head) {
+	//TC:O(n)   SC:(1)
+	public static boolean hasCycle3(ListNode head) {
 		ListNode slow, fast;
 		slow = fast = head;
-		while (current != null) {
+		do {
 			slow = slow.next;
+			if(fast == null || fast.next == null) return false;
 			fast = fast.next.next;
-		}
-		return false;
+		} while (slow != fast);
+		return true;
 	}
 
 	public static void main(String[] args) {
