b5ce0d4c059831c494984271983dbea7ec4186cb
Algorithmica <info@algorithmica.co.in>
2019-06-22 12:32:48 +0530

    Add files via upload

---
+++ 2019-may/7.dynamic programming/src/com/alg/top20/dp/MaxNonConsSum.java
@@ -0,0 +1,74 @@
+package com.alg.top20.combinatorics;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MaxNonConsSum {
+
+	public static int maxNonConsSum1(int[] in) {
+		MyInteger gmax = new MyInteger(0);
+		auxNonConsSum1(0, 0, gmax, in);
+		return gmax.get();
+	}
+	
+	private static void auxNonConsSum1(int i, int psum, MyInteger gmax, int[] in) {
+		if(i >= in.length) {
+			if(psum > gmax.get())
+				gmax.set(psum);
+			return;
+		}
+		auxNonConsSum1(i+2, psum+in[i], gmax, in);
+		auxNonConsSum1(i+1, psum, gmax, in);
+	}
+	
+	public static int maxNonConsSum2(int[] in) {
+		return auxNonConsSum2(0, in);
+	}
+	private static int auxNonConsSum2(int i, int[] in) {
+		if(i >= in.length) return 0;
+		int inclusive = auxNonConsSum2(i+2, in);
+		int exclusive = auxNonConsSum2(i+1, in);
+		return Math.max(inclusive + in[i], exclusive);
+	}
+	
+	public static int maxNonConsSum3(int[] in) {
+		int[] mem =  new int[in.length];     
+		auxNonConsSum3(0, in, mem);
+		return mem[0];
+	}
+	private static int auxNonConsSum3(int i, int[] in, int[]  mem) {
+		if(i >= in.length) return 0;
+		if(mem[i] != 0) return mem[i];
+		int inclusive = auxNonConsSum3(i+2, in, mem);
+		int exclusive = auxNonConsSum3(i+1, in, mem);
+		mem[i] = Math.max(inclusive + in[i], exclusive);
+		return mem[i];
+	}
+	
+	public static int maxNonConsSum4(int[] in) {
+		int[] mem =  new int[in.length+2];
+		mem[in.length+1] = mem[in.length] = 0;
+		for(int i = in.length-1; i >= 0; --i) {
+			int inclusive = mem[i+2];
+			int exclusive = mem[i+1];
+			mem[i] = Math.max(inclusive + in[i], exclusive);
+		}
+		return mem[0];
+	}
+
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int[] in = new int[n];
+		Random r = new Random();
+		for(int i = 0; i < n; ++i)
+			in[i] = r.nextInt(10);
+		System.out.println(Arrays.toString(in));
+		//System.out.println(maxNonConsSum1(in));
+		//System.out.println(maxNonConsSum2(in));
+		//System.out.println(maxNonConsSum3(in));
+		System.out.println(maxNonConsSum4(in));
+
+		
+	}
+
+}
---
+++ 2019-may/7.dynamic programming/src/com/alg/top20/dp/MyInteger.java
@@ -0,0 +1,20 @@
+package com.alg.top20.combinatorics;
+
+public class MyInteger {
+	private int value;
+
+	public MyInteger(int value) {
+		super();
+		this.value = value;
+	}
+	public int get() {
+		return value;
+	}
+	public void set(int value) {
+		this.value = value;
+	}
+	public void incr() {
+		++this.value;
+	}
+
+}
